d5b6a207283327f5885386ccfbe6427454053fe6
pbiology <anders.lind@scilifelab.se>
2026-04-10 12:47:27 +0200

    Updated author and name

--- docs/conf.py
+++ docs/conf.py
@@ -9,9 +9,9 @@ from pathlib import Path
 sys.path.insert(0, str(Path(__file__).parent.parent / "backend"))
 
 # -- Project information
-project = "meta-vis-app"
-copyright = "2026, Genomic Medicine Sweden"
-author = "Genomic Medicine Sweden"
+project = "meta-vis"
+copyright = "2026, Anders Lind"
+author = "Anders Lind"
 release = "0.1.0"
 
 # -- General configuration
