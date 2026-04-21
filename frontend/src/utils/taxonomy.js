6801209ac5dd7379ee5a4ac610c9c149b58e8456
pbiology <anders.lind@scilifelab.se>
2026-04-21 11:08:03 +0200

    Taxonomy constants

---
+++ frontend/src/utils/taxonomy.js
@@ -0,0 +1,9 @@
+// NCBI taxonomy IDs used across the app.
+// See https://www.ncbi.nlm.nih.gov/taxonomy
+export const TAXON_ID_UNCLASSIFIED = 0;
+export const TAXON_ID_ROOT = 1;
+export const TAXON_ID_HUMAN = 9606; // Homo sapiens
+export const TAXON_ID_BACTERIA = 2;
+export const TAXON_ID_EUKARYOTA = 2759;
+export const TAXON_ID_ARCHAEA = 2157;
+export const TAXON_ID_VIRUSES = 10239;
