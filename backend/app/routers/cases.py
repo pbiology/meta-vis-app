2f0a28e44288f2687f5a1886035e4dac59e3a46e
pbiology <anders.lind@scilifelab.se>
2026-04-21 12:36:41 +0200

    Get ticket URL

--- backend/app/routers/cases.py
+++ backend/app/routers/cases.py
@@ -26,6 +26,9 @@ def _serialise_case(doc: dict) -> dict:
     doc["_id"] = str(doc["_id"])
     if "sample_ids" in doc:
         doc["sample_ids"] = [str(sid) for sid in doc["sample_ids"]]
+    ticket_id = doc.get("ticket_id")
+    if ticket_id and settings.freshdesk_base_url:
+        doc["ticket_url"] = settings.freshdesk_base_url.format(ticket_id=ticket_id)
     return doc
 
 
