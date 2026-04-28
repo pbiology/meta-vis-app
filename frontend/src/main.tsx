03aa6b94f4e460eafa15521f4a6ee05c528d7bc7
pbiology <anders.lind@scilifelab.se>
2026-04-28 23:56:06 +0200

    Read from endpoint

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
+import { ConfigProvider } from "./context/ConfigContext";
 import queryClient from "./queryClient";
 import "./index.css";
 
@@ -14,9 +15,11 @@ ReactDOM.createRoot(rootEl).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
+        <ConfigProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </ConfigProvider>
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
